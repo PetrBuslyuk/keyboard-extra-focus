863627825c022cd04fa97645d01796df6940f28e
Petr Buslyuk <skyliner270594@mail.ru>
2017-10-26 23:15:45 +0300

    add first version of directive

---
+++ keyboard-extra-focus.js
@@ -0,0 +1,88 @@
+/**
+ * Created by Petr Buslyuk on 26.10.17.
+ */
+
+'use strict';
+
+(function () {
+    keyboardExtraFocus.$inject = ['$rootScope', '$document'];
+
+    function keyboardExtraFocus($rootScope, $document) {
+
+        var KEYS;
+
+        return {
+            restrict: 'A',
+            scope: {
+                extraOptions: '='
+            },
+            link: link
+        };
+
+        function link(scope, element) {
+            const FOCUS_CLASS = 'keyboard-class';
+            var activeDOMElement;
+
+            scope.extraOptions = scope.extraOptions || {};
+
+            // By default handle only TAB Key
+            KEYS = scope.extraOptions.keys || [9];
+
+            element.on('mousedown', onBlur);
+            element.on('keyup', onKeyUp);
+
+            $rootScope.$on('moveFocusTo', onMoveFocusTo);
+
+            function onKeyUp(e) {
+                var code = e.keyCode || e.which;
+
+                if(scope.extraOptions.keys.indexOf(code) !== -1)  {
+                    addExtraClass(e);
+                }
+            }
+
+            function onMoveFocusTo(ev, data) {
+                var focusElement;
+
+                onBlur();
+
+                if (data.query) {
+                    focusElement = $document.querySelector(data.query);
+                }
+
+                if (data.element) {
+                    focusElement = data.element;
+                }
+
+                if (focusElement) {
+                    focusElement.focus();
+                }
+
+                if(focusElement && !data.skipExtraClass) {
+                    addExtraClass(ev, focusElement);
+                }
+            }
+
+            function addExtraClass(e, customElement) {
+                activeDOMElement = angular.element(customElement || e.target);
+
+                if(!activeDOMElement) return false;
+
+                activeDOMElement.addClass(getExtraClass);
+                activeDOMElement.on('blur', onBlur)
+            }
+
+            function onBlur() {
+                if (!activeDOMElement) return false;
+
+                activeDOMElement.removeClass(getExtraClass);
+                activeDOMElement.off('blur', onBlur);
+            }
+
+            function getExtraClass() {
+                return scope.extraOptions.focusClass || FOCUS_CLASS;
+            }
+
+        }
+    }
+})();
